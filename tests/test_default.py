42fe6e43a6a298a15e53ee92db29d661dc585188
Simon Li <orpheus+devel@gmail.com>
2017-04-18 21:41:33 +0100

    Update tests to work with prometheus docker

--- tests/test_default.py
+++ tests/test_default.py
@@ -2,24 +2,32 @@ import testinfra.utils.ansible_runner
 import pytest
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('prometheus')
+    '.molecule/ansible_inventory').get_hosts('all')
 
 
-@pytest.mark.parametrize("name", [
-    "prometheus",
-    "prometheus-alertmanager",
-    "prometheus-node-exporter",
-])
-def test_services_running_and_enabled(Service, name):
-    assert Service(name).is_running
-    assert Service(name).is_enabled
+def test_docker_running(Sudo, Command):
+    out = Command.check_output("docker ps --format '{{.Names}}'")
+    names = sorted(out.split())
+    assert names == ['alertmanager', 'blackbox-exporter', 'prometheus']
+
+
+def test_docker_volumes(Sudo, Command):
+    out = Command.check_output("docker volume ls --format '{{.Name}}'")
+    names = sorted(out.split())
+    assert names == ['alertmanager-data', 'prometheus-data']
 
 
 @pytest.mark.parametrize("address", [
     "http://localhost:9090",
     "http://localhost:9093",
-    "http://localhost:9100",
+    "http://localhost:9115",
 ])
 def test_prometheus_metrics(Command, address):
     out = Command.check_output('curl %s/metrics' % address)
     assert 'process_cpu_seconds_total' in out
+
+
+def test_prometheus_targets(Command):
+    out = Command.check_output('curl http://localhost:9090/targets')
+    assert 'http://localhost:9090/metrics' in out
+    assert 'http://blackbox-exporter:9115/probe' in out
